33d752438bfcbfa0055783bd7a8116ac9c089395
csteach424 <csteach424@gmail.com>
2019-04-08 22:23:18 -0500

    update source for week 13

---
+++ extras/oauth/google/test-oauth2-profile/test-oauth.js
@@ -0,0 +1,24 @@
+/**
+* test-oauth.js
+*  - test oauth2 with js
+*  - get profile
+*  - offer explicit app-only logout 
+*/
+
+//get profile information for a google sign in
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
+//explicit app only signout from google account
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
