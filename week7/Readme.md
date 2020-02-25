92313651b3ac2d7970658b2e53dc1e7774df41ac
ancientlives <ancientlives@gmail.com>
2020-02-24 20:41:36 -0600

    update source for week 7

---
+++ week7/Readme.md
@@ -0,0 +1,3 @@
+### Source - Week 7 - JavaScript
+
+JavaScript source code examples for week 7 of the COMP 424 course.
---
+++ week7/travelnotes-plainjs/plainjs-4/plainjs.js
@@ -0,0 +1,39 @@
+/*
+* travel notes JS
+* - plain JS example usage...
+*/
+
+function travelNotes() {
+  "use strict";
+
+  // get a reference to `.note_output` in the DOM
+  let noteOutput = document.querySelector('.note-output');
+  // add note button
+  let addNoteBtn = document.getElementById('add-note');
+  // input field for add note
+  let inputNote = document.getElementById('input-note');
+
+  // add event listener to add note button
+  addNoteBtn.addEventListener('click', () => {
+    // create p node
+    let p = document.createElement('p');
+    // get value from input field for note
+    let inputVal = inputNote.value;
+
+    // check input value
+    if (inputVal !== '') {
+      // create text node
+      let noteText = document.createTextNode(inputVal);
+      // append text to paragraph
+      p.appendChild(noteText);
+      // append new paragraph and text to existing note output
+      noteOutput.appendChild(p);
+      // clear input text field
+      inputNote.value = '';
+    }
+  });
+
+}
+
+// load app
+travelNotes();
